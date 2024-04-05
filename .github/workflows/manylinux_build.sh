84f340839625daf69dc20eeb9e5dd0e45fd8f5c2
Gabe Fierro <gtfierro@mines.edu>
2024-04-05 17:40:14 -0600

    using build scripts from oxigraph

---
+++ .github/workflows/manylinux_build.sh
@@ -0,0 +1,25 @@
+# from https://github.com/oxigraph/oxigraph/blob/main/.github/workflows/manylinux_build.sh
+cd /workdir
+yum -y install centos-release-scl-rh
+yum -y install llvm-toolset-7.0
+source scl_source enable llvm-toolset-7.0
+curl https://static.rust-lang.org/rustup/dist/%arch%-unknown-linux-gnu/rustup-init --output rustup-init
+chmod +x rustup-init
+./rustup-init -y --profile minimal
+source "$HOME/.cargo/env"
+export PATH="${PATH}:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
+cd python
+python3.12 -m venv venv
+source venv/bin/activate
+pip install -r requirements.dev.txt
+maturin develop --release
+python generate_stubs.py ontoenv ontoenv.pyi --ruff
+maturin build --release --features abi3 --compatibility manylinux2014
+if [ %for_each_version% ]; then
+  for VERSION in 8 9 10 11 12; do
+    maturin build --release --interpreter "python3.$VERSION" --compatibility manylinux2014
+  done
+  for VERSION in 9 10; do
+    maturin build --release --interpreter "pypy3.$VERSION" --compatibility manylinux2014
+  done
+fi
