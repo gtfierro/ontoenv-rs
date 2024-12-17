3da3cc43e15853b5605b9c347ee704b3ceeba73c
Gabe Fierro <gtfierro@mines.edu>
2024-12-17 11:25:20 -0700

    nightly build on manyu/musllinux builds

--- .github/workflows/manylinux_build.sh
+++ .github/workflows/manylinux_build.sh
@@ -5,7 +5,7 @@ yum -y install llvm-toolset-7.0
 source scl_source enable llvm-toolset-7.0
 curl https://static.rust-lang.org/rustup/dist/%arch%-unknown-linux-gnu/rustup-init --output rustup-init
 chmod +x rustup-init
-./rustup-init -y --profile minimal
+./rustup-init -y --profile minimal --default-toolchain nightly
 source "$HOME/.cargo/env"
 export PATH="${PATH}:/opt/python/cp37-cp37m/bin:/opt/python/cp38-cp38/bin:/opt/python/cp39-cp39/bin:/opt/python/cp310-cp310/bin:/opt/python/cp311-cp311/bin"
 cd python
